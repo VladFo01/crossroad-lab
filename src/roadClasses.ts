ada7debe242b5f1787d19dc5a919e5371d7bdfff
Kot Leta <chvin708@gmail.com>
2022-10-19 00:29:06 +0300

    Fixed array issues, edited existing classes

--- src/roadClasses.ts
+++ src/roadClasses.ts
@@ -1,6 +1,6 @@
 namespace RoadClasses {
     class Cell {
-        protected isOccupied: boolean;
+        public isOccupied: boolean;
         protected isRideable: boolean;
         protected hasCrosswalk: boolean;
         constructor(hasCrosswalk: boolean, occ: boolean, ride: boolean) {
@@ -20,8 +20,8 @@ namespace RoadClasses {
         get Rideability(): boolean {
             return this.isRideable;
         }
-        set Crosswalk(occ: boolean) {
-            this.isOccupied = occ;
+        set Crosswalk(has: boolean) {
+            this.hasCrosswalk = has;
         }
         get Crosswalk(): boolean {
             return this.hasCrosswalk;
@@ -29,55 +29,78 @@ namespace RoadClasses {
     }
 
     interface ofCell {
-        massive: Cell[][];
+        Matrix: Cell[][];
     }
 
     enum conDirection { Vertical, Horizontal };
 
     class Crosswalk {
-        private massive: Cell[];
-        constructor(connection:Cell[][], direction: conDirection) {
-            this.massive = [new Cell(true, true, false), new Cell(true, true, false)];
-            if (direction == conDirection.Vertical) {
-                //fill smlike
+        //massive: Cell[];
+        constructor(Matrix: Cell[][], x: number, y: number, direction: conDirection, length: number) {
+            //this.massive = [new Cell(true, true, false), new Cell(true, true, false)];
+            if (direction == conDirection.Horizontal) {
+                Matrix[y][x].Crosswalk = true;
+                Matrix[y + 1][x].Crosswalk = true;
+                Matrix[y][x + length].Crosswalk = true;
+                Matrix[y + 1][x + length].Crosswalk = true;
             }
             else {
-                //fill smlike
+                Matrix[y][x].Crosswalk = true;
+                Matrix[y][x + 1].Crosswalk = true;
+                Matrix[y + length][x].Crosswalk = true;
+                Matrix[y + length][x + 1].Crosswalk = true;
             }
         }
     }
 
-    class Connection implements ofCell {
-        massive: Cell[][];
+    class Connection {
+        //massive: Cell[][];
         crosswalk: Crosswalk;
         constructor(Matrix: Cell[][], x: number, y: number, direction: conDirection, length: number) {
-            this.crosswalk = new Crosswalk(this.massive, direction);
+            this.crosswalk = new Crosswalk(Matrix, x, y, direction, length);
             // to do
+            if (direction == conDirection.Horizontal) {
+                for (var i = x; i < x + length; i++) {
+                    Matrix[y][i].Rideability = true;
+                    Matrix[y + 1][i].Rideability = true;
+                }
+            }
+            else {
+                for (var i = y; i < y + length; i++) {
+                    Matrix[i][x].Rideability = true;
+                    Matrix[i][x + 1].Rideability = true;
+                }
+            }
         }
     }
 
-    export class RoadMatrix { // Facade
+    export class RoadMatrix implements ofCell { // Facade
         MatrixSize: number;
         Matrix: Cell[][];
-        Highway: Connection;
+        Highway: Connection[];
 
         constructor(size: number) {
             this.MatrixSize = size;
+            this.Matrix = new Array(0);
+            this.Highway = new Array(0);
             for (var i = 0; i < this.MatrixSize; i++) {
+                this.Matrix.push([]);
                 for (var j = 0; j < size; j++) {
                     this.Matrix[i][j] = new Cell(false, false, false);
                 }
             }
-            this.Highway = new Connection(this.Matrix, 10, 10, conDirection.Horizontal, 20);
+            this.Highway[0] = new Connection(this.Matrix, 0, 0, conDirection.Horizontal, 40);
+            this.Highway[1] = new Connection(this.Matrix, 5, 5, conDirection.Vertical, 40);
         }
 
         Check(): boolean {
             // will be done
             return true;
         }
-    }
+    } 
+
 }
 
 const roadMatrix = new RoadClasses.RoadMatrix(50);
-console.log(roadMatrix.Matrix[0][0].Occupation);
-
+for (var i = 0; i < roadMatrix.MatrixSize; i++)
+    console.log(roadMatrix.Matrix[i][i].isOccupied);
