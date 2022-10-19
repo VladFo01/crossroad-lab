58706f4b842ecfd20f006e177bd6cf94dc08876d
Vlad Foroshcha <vladfo2003@gmail.com>
2022-10-20 00:41:24 +0300

    feat: adjust eslint rules + set commonjs type

--- src/trafficParticipants/EntitySpawner.ts
+++ src/trafficParticipants/EntitySpawner.ts
@@ -20,20 +20,20 @@ class EntitySpawner {
         let entity: RoadUser;
 
         switch (id) {
-        case 1:
-            entity = new Pedestrian(this.pedestrianVel, dir);
-            break;
-        case 2:
-            entity = new Vehicle(this.vehicleVel, dir);
-            break;
-        case 3:
-            entity = new SpecialTransport(this.specialVel, dir);
-            break;
-        default:
-            console.error(`Invalid data: id must be in scope of 1-3, your id is ${id}`);
-            break;
+            case 1:
+                entity = new Pedestrian(this.pedestrianVel, dir);
+                break;
+            case 2:
+                entity = new Vehicle(this.vehicleVel, dir);
+                break;
+            case 3:
+                entity = new SpecialTransport(this.specialVel, dir);
+                break;
+            default:
+                console.error(`Invalid data: id must be in scope of 1-3, your id is ${id}`);
+                break;
         }
 
-    return entity;
-  }
+        return entity;
+    }
 }
