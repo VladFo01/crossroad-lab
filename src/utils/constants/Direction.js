90abd637aebc89ecaeb5e8a21bab397e97b027af
Kot Leta <chvin708@gmail.com>
2022-10-25 18:12:46 +0300

    Feat: deleted odd files

--- src/utils/constants/Direction.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.conDirection = void 0;
-var conDirection;
-(function (conDirection) {
-    conDirection[conDirection["Vertical"] = 0] = "Vertical";
-    conDirection[conDirection["Horizontal"] = 1] = "Horizontal";
-})(conDirection = exports.conDirection || (exports.conDirection = {}));
-;
