feef2b513202146c90dd9b6d74ac2aa8e79266f6
Kot Leta <chvin708@gmail.com>
2022-10-25 18:09:54 +0300

    Feat: added utils file, created file with enums there

---
+++ src/utils/constants/Direction.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.conDirection = void 0;
+var conDirection;
+(function (conDirection) {
+    conDirection[conDirection["Vertical"] = 0] = "Vertical";
+    conDirection[conDirection["Horizontal"] = 1] = "Horizontal";
+})(conDirection = exports.conDirection || (exports.conDirection = {}));
+;
