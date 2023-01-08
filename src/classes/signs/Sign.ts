9fa685b56fae084828b8cd57e5bb36912e7470be
Vlad Foroshcha <vladfo2003@gmail.com>
2023-01-08 12:18:56 +0200

    feat: implement SignForInteraction class

--- src/classes/signs/Sign.ts
+++ src/classes/signs/Sign.ts
@@ -5,22 +5,14 @@ import { RoadUser } from '../trafficParticipants/RoadUser';
 export interface SignProps {
   cell: Cell;
   image?: string;
-  isFor?: SignIsFor;
 }
 
 export class Sign {
   protected cell: Cell;
-  protected isFor?: SignIsFor;
   protected image?: string;
 
-  constructor({ cell, image, isFor }: SignProps) {
+  constructor({ cell, image }: SignProps) {
     this.cell = cell;
     this.image = image;
-    this.isFor = isFor;
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  public callback(roadUser: RoadUser): RoadUser {
-    return roadUser;
   }
 }
